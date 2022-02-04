8545dda993852633a0c68cbddb2807e6026cdcf7
Lupita Peralta <lupitaaperalta@gmail.com>
2022-02-04 15:49:07 -0500

    Update with the patient-model.js schema

---
+++ server/models/patient-model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Item = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        patientId: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        sex: {
+            type: String,
+            required: true
+        },
+        race: {
+            type: String,
+            required: true
+        },
+        zip: {
+            type: Number,
+            required: true
+        },
+        latestBMI: {
+            type: Number,
+            required: true
+        },
+        latestWeight: {
+            type: Number,
+            required: true
+        },
+        latestHeight: {
+            type: Number,
+            required: true
+        },
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model('item', Item);
