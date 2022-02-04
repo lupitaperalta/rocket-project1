04f179801fc777b849edf30afc17ae83bdc391da
Abdu Hosein <ahosein57@gmail.com>
2022-02-04 17:12:53 -0500

    Added exam data model

---
+++ server/models/exam-model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Exam = new Schema(
+    {
+        exam_id: new Schema.Types.ObjectId,
+        patient_id: new Schema.Types.ObjectId,
+        image: {
+            type: String,
+            required: true
+        },
+        key_findings: {
+            type: String,
+            required: false
+        },
+        brexia_score: {
+            type: Number,
+            required: false
+        },
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model('exam', Exam);
