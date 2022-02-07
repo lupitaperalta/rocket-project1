403d2bda29dc47c9ce43475141856ce29a110b92
Abdu Hosein <87817813+AbduHosein@users.noreply.github.com>
2022-02-07 16:48:44 -0500

    Added patient route

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Require controller module for exams
 var exam_controller = require('../controllers/exam-controller');
 
-// Exams home page 
+// Patient page 
 router.get('/', (req, res) => {
     res.send('Currently on the patient page')
 });
