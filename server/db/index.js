8c60ef0e3aedf5d86a58553e04fb1cd45d9e344a
Abdu Hosein <87817813+AbduHosein@users.noreply.github.com>
2022-02-07 16:45:15 -0500

    Imported exam model in index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,6 @@
 const { handle } = require('express/lib/application');
 const mongoose = require('mongoose');
+// Import exam model
 const model = require('../models/exam-model.js')
 mongoose
   .connect('mongodb://127.0.0.1:27017/items', /*{ useNewUrlParser: true, useFindAndModify: false }*/)
