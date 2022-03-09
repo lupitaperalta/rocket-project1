d4cabc3bd15257865c42a805dd01f1b1158abe35
lupitaperalta <89276351+lupitaperalta@users.noreply.github.com>
2022-03-09 15:54:24 -0500

    connected to mongo atlas

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,8 @@ const examModel = require('../models/exam-model.js');
 const patientModel = require('../models/patient-model');
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/items', { useNewUrlParser: true, useFindAndModify: false })
+  //.connect('mongodb://127.0.0.1:27017/items', { useNewUrlParser: true, useFindAndModify: false })
+  .connect('mongodb+srv://lupitaperalta:Lokkol11@cluster0.spxa9.mongodb.net/hackCovidTest?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true ,useFindAndModify: false })
   .catch(e => {
     console.error('Connection error', e.message);
   });
