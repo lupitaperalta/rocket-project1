e344b3740c0d4e9e582e3bfbc8cfdb9874d15227
lupitaperalta <89276351+lupitaperalta@users.noreply.github.com>
2022-03-12 16:58:07 -0500

    Add files via upload

---
+++ client/src/pages/ItemInsert.js
@@ -0,0 +1,199 @@
+import React, { Component } from 'react';
+import { shared } from '../constants';
+import api from '../api';
+
+import styled from 'styled-components';
+
+const Title = styled.h1.attrs({
+  className: 'h1',
+})``;
+
+const Wrapper = styled.div.attrs({
+  className: 'form-group',
+})`
+  margin-top: 0 30px;
+`;
+
+const Label = styled.label`
+  margin: 5px;
+  max-width: 30%;
+
+  @media screen and (max-width: 420px) {
+    height: auto;
+    max-width: 75%;
+  }
+`;
+
+const InputText = styled.input.attrs({
+  className: 'form-control',
+})`
+  margin: 5px auto;
+  max-width: 30%;
+  text-align: center;
+
+  @media screen and (max-width: 420px) {
+    height: auto;
+    max-width: 75%;
+  }
+`;
+
+const Fieldset = styled.fieldset.attrs({
+  className: 'form-control',
+})`
+  background-color: transparent;
+  border-color: transparent;
+  margin: 1em auto 0.5em;
+  max-width: 50%;
+  min-height: 6em;
+
+  @media screen and (max-width: 420px) {
+    height: auto;
+    max-width: 75%;
+  }
+`;
+
+const DayInput = styled.input.attrs({
+  className: '',
+})`
+  margin: 5px 5px 5px auto;
+  text-align: center;
+`;
+
+const Button = styled.button.attrs({
+  className: 'btn btn-primary',
+})`
+  margin: 15px 15px 15px 5px;
+`;
+
+// if you majorly fuck up all of the item pages in pages are the originals
+const CancelButton = styled.a.attrs({
+  className: 'btn btn-danger',
+})`
+  margin: 15px 15px 15px 5px;
+`;
+
+class ItemInsert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      examName: '',
+      patientName: '',
+      image: '',
+      keyFindings: '',
+      brixiaScore: '',
+    };
+  }
+
+  handleChangeInputExamName = async event => {
+    const examName = event.target.value;
+    this.setState({ examName });
+  };
+  handleChangeInputPatientName = async event => {
+    const patientName = event.target.value;
+    this.setState({ patientName });
+  };
+  handleChangeInputImage = async event => {
+    const image = event.target.value;
+    this.setState({ image });
+  };
+  handleChangeInputKeyFindings = async event => {
+    const keyFindings = event.target.value;
+    this.setState({ keyFindings });
+  };
+  handleChangeInputBrixiaScore = async event => {
+    const brixiaScore = event.target.validity.valid ? event.target.value : this.state.brixiaScore;
+
+    this.setState({ brixiaScore });
+  };
+
+
+  insertSingleItem = item => {
+    return api
+      .insertItem(item)
+      .then(resp => {
+        console.log('insertItem: resp');
+        console.log(resp);
+        if ((resp.data || {}).success) {
+          const newItem = JSON.parse(resp.config.data);
+          console.log('insertItem: newItem', newItem);
+        }
+        return resp;
+      })
+      .catch(err => {
+        console.error(`ERROR in 'insertSingleItem': ${err}`);
+        console.error(err);
+        return err;
+      });
+  };
+
+  handleInsertItem = event => {
+    event.preventDefault();
+
+    const { examName, patientName, image, keyFindings, brixiaScore } = this.state;
+    const item = { examName, patientName, image, keyFindings, brixiaScore };
+
+    this.insertSingleItem(item)
+      .then(resp => {
+        console.log('handleInsertItem: resp');
+        console.log(resp);
+        if (typeof resp === 'object' && resp.status < 300 && resp.status >= 200) {
+          window.alert('Item inserted successfully');
+          this.setState({
+            examName: '',
+            patientName: '',
+            image: '',
+            keyFindings: '',
+            brixiaScore: 0,
+          });
+        } else {
+          throw resp;
+        }
+      })
+      .catch(err => {
+        // TODO: pass error object correctly so that things like validation errors can be displayed to user
+        window.alert(`There was an error creating the item... :(`);
+        console.log('handleInsertItem: err');
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { examName, patientName, image, keyFindings, brixiaScore } = this.state;
+
+
+    return (
+      <Wrapper>
+        <Title>Create Item</Title>
+
+        <Label>Exam ID: </Label>
+        <InputText type="text" value={examName} onChange={this.handleChangeInputExamName} />
+
+        <Label>Patient ID: </Label>
+        <InputText type="text" value={patientName} onChange={this.handleChangeInputPatientName} />
+
+        <Label>Image: </Label>
+        <InputText type="text" value={image} onChange={this.handleChangeInputImage} />
+
+        <Label>Key Findings: </Label>
+        <InputText type="text" value={keyFindings} onChange={this.handleChangeInputKeyFindings} />
+
+        <Label>Brixia Score: </Label>
+        <InputText
+          type="number"
+          step="0.1"
+          lang="en-US"
+          min="0"
+          max="1000"
+          pattern="[0-9]+([,\.][0-9]+)?"
+          value={brixiaScore}
+          onChange={this.handleChangeInputBrixiaScore}
+        />
+
+        <Button onClick={this.handleInsertItem}>Add Item</Button>
+        <CancelButton href={'/items'}>Cancel</CancelButton>
+      </Wrapper>
+    );
+  }
+}
+
+export default ItemInsert;
---
+++ client/src/pages/ItemsTable.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ReactTable from 'react-table-6';
+import { DeleteButton } from '../components/buttons';
+import api from '../api';
+
+import styled from 'styled-components';
+
+import 'react-table-6/react-table.css';
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`;
+
+class ItemsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {},
+    };
+  }
+
+  componentDidMount() {
+    console.log('ItemsList: props');
+    console.log(this.props);
+    // if (((this.props.itemData || {}).items || []).length) return;
+
+    this.fetchAllItems();
+  }
+
+  fetchAllItems = () => {
+    api
+      .getAllItems()
+      .then(resp => {
+        debugger;
+        const { items } = resp.data;
+        console.log('getAllItems: resp');
+        console.log(items);
+        this.setState({ items });
+      })
+      .catch(err => {
+        console.error(`ERROR in 'getAllItems': ${err}`);
+        console.error(err);
+        return err;
+      });
+  };
+
+  deleteSingleItem = itemId => {
+    return api
+      .deleteItemById(itemId)
+      .then(resp => {
+        console.log('deleteItemById: resp');
+        console.log(resp);
+        return resp;
+      })
+      .catch(err => {
+        console.error(`ERROR in 'deleteSingleItem': ${err}`);
+        console.error(err);
+        return err;
+      });
+  };
+
+  handleRemoveItem = data => {
+    const itemId = data;
+
+    this.deleteSingleItem(itemId).then(resp => {
+      console.log('handleRemoveItem: resp');
+      console.log(resp);
+      this.fetchAllItems();
+    });
+  };
+
+  render() {
+    const items = this.state.items || {};
+    console.log(items);
+
+    const columns = [
+      {
+        Header: 'ID',
+        accessor: '_id',
+        filterable: true,
+        Cell: props => {
+          return <span data-item-id={props.original._id}>{props.original._id}</span>;
+        },
+      },
+      {
+        Header: 'Exam ID',
+        accessor: 'examName',
+        filterable: true,
+        Cell: props => {
+          return <span data-examName={props.original.examName}>{props.value}</span>;
+        },
+      },
+      {
+        Header: 'Patient ID',
+        accessor: 'patientName',
+        Cell: props => {
+          return <span data-patientName={props.original.patientName}>{props.value || '-'}</span>;
+        },
+      },
+      {
+        Header: 'Image',
+        accessor: 'image',
+        Cell: props => {
+          return <span data-image={props.original.image}>{props.value || '-'}</span>;
+        },
+      },
+      {
+        Header: 'Key Findings',
+        accessor: 'keyFindings',
+        Cell: props => {
+          return <span data-keyFindings={props.original.keyFindings}>{props.value || '-'}</span>;
+        },
+      },
+      {
+        Header: 'Brixia Score',
+        accessor: 'brixiaScore',
+        filterable: true,
+        Cell: props => {
+          return <span data-brixiaScore={props.original.brixiaScore}>{props.value}</span>;
+        },
+      },
+      {
+        Header: '',
+        accessor: '',
+        Cell: props => {
+          return (
+            <Link data-update-id={props.original._id} to={`/item/update/${props.original._id}`}>
+              Update Item
+            </Link>
+          );
+        },
+      },
+      {
+        Header: '',
+        accessor: '',
+        Cell: props => {
+          return (
+            <span data-delete-id={props.original._id}>
+              <DeleteButton id={props.original._id} onDelete={this.handleRemoveItem} />
+            </span>
+          );
+        },
+      },
+    ];
+
+    return (
+      <Wrapper>
+        {(items || []).length > 0 ? ( // defeats the purpose of using `isLoading` prop?
+          <ReactTable
+            data={items}
+            columns={columns}
+            defaultPageSize={10}
+            showPageSizeOptions={true}
+            minRows={10}
+          />
+        ) : (
+          `No items to render!!! :(`
+        )}
+      </Wrapper>
+    );
+  }
+}
+
+export default ItemsList;
