37f7a02aeca3acfb7da79e4c343e4d1a3016eb68
lupitaperalta <89276351+lupitaperalta@users.noreply.github.com>
2022-03-12 17:25:54 -0500

    Add files via upload

---
+++ server/routes/exam-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const ItemController = require('../controllers/exam-controller');
+
+const router = express.Router();
+
+router.get('/exams', ItemController.getItems);
+router.get('/exam/:id', ItemController.getItemById);
+router.post('/exam', ItemController.createItem);
+router.put('/exam/:id', ItemController.updateItem);
+router.delete('/exam/:id', ItemController.deleteItem);
+
+
+module.exports = router;
